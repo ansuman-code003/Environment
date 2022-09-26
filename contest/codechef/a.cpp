fcc0350c294062bf8bc47f942efe8de78cae1e95
ansuman-code003 <mazumder.code003@gmail.com>
2022-09-26 21:57:04 +0530

    Added contest folder for various CP based online competitions.

---
+++ contest/codechef/a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void test_case(){
+	/* Solve Here!	*/
+	
+}
+
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	int test=1; //cin >> test;
+	while(test--) test_case();
+	return 0;
+}
